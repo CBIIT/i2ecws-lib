9bb45e95033fab7ebe0950215a9be57543a2eb00
kanozad <doug.kanoza@gmail.com>
2025-03-31 11:45:46 -0400

    saving generation scripts

---
+++ generate-gpmats.ps1
@@ -0,0 +1,23 @@
+$Env:JAVA_HOME="C:\Users\kanozad\dev\java\jdk17.0.14_7";
+
+$output_path="projects\gpmats-itp\java-client" 
+
+$swagger_opts="-i api\gpmats-itp.json"
+
+$template_opts="-t templates\Java"
+
+$config_opts="-c .\config.json"
+
+$output_opts="-o $output_path"
+
+$lang_opts="-l java"
+
+$sys_opts=
+
+$misc_opts=
+
+# Write-Output "java -jar bin\swagger-codegen-cli-3.0.68.jar generate $swagger_opts $template_opts $lang_opts $output_opts $config_opts $misc_opts"
+
+Remove-Item -Force -Recurse $output_path
+
+Invoke-Expression "java -jar bin\swagger-codegen-cli-3.0.68.jar generate $swagger_opts $template_opts $lang_opts $output_opts $config_opts $misc_opts"
