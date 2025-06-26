78c21ff117dd7812d6bf5afa30a9c6dd327f7798
kanozad <doug.kanoza@gmail.com>
2025-06-26 15:08:36 -0400

    Added draft of NIDAP swagger specification Shiva - ignore this. It's my pet swagger project.

--- doc/lang-options.ps1
+++ doc/lang-options.ps1
@@ -1,3 +1,3 @@
 $Env:JAVA_HOME="C:\Users\kanozad\dev\java\jdk17.0.10_7";
 
-java -jar C:\Users\kanozad\dev\workspaces\swagger-codegen-workspace\i2ecws-lib\bin\swagger-codegen-cli-3.0.68.jar config-help -l typescript-angular
+java -jar C:\Users\kanozad\dev\workspaces\swagger-codegen-workspace\i2ecws-lib\bin\swagger-codegen-cli-3.0.68.jar config-help -l java
---
+++ generate-icdata-server.ps1
@@ -0,0 +1,23 @@
+$Env:JAVA_HOME="C:\Users\kanozad\dev\java\jdk17.0.14_7";
+
+$output_path="projects\icdata\java-server"
+
+$swagger_opts="-i api\icdata.json"
+
+$template_opts="-t templates\JavaSpring"
+
+$config_opts="-c .\config-icdata-server.json"
+
+$output_opts="-o $output_path"
+
+$lang_opts="-l spring"
+
+$sys_opts=
+
+$misc_opts=
+
+# Write-Output "java -jar bin\swagger-codegen-cli-3.0.68.jar generate $swagger_opts $template_opts $lang_opts $output_opts $config_opts $misc_opts"
+
+Remove-Item -Force -Recurse $output_path -Confirm:$false
+
+Invoke-Expression "java -jar bin\swagger-codegen-cli-3.0.68.jar generate $swagger_opts $template_opts $lang_opts $output_opts $config_opts $misc_opts"
